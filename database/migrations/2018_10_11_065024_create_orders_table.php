e9d0ddfe6cc8349663790cb39be30d0ee5e532d6
Bhaven Kakade <thisisbhaven@gmail.com>
2018-10-11 12:32:36 +0530

    tables restructured

--- database/migrations/2018_10_11_065024_create_orders_table.php
+++ database/migrations/2018_10_11_065024_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
